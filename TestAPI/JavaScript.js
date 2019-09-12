2590601cab132efe8362f32cf9615d671ec793c3
Kayl Eubanks <kayleubanks@gmail.com>
2019-09-12 15:27:39 -0700

    Create example HTML file using endpoints created in controller file

---
+++ TestAPI/JavaScript.js
@@ -0,0 +1,23 @@
+﻿var uri = 'api/products';
+
+$(document).ready(function () {
+    $.getJSON(uri).done(function (data) {
+        $.each(data, function (key, item) {
+            $('<li>', { text: formatItem(item) }).appendTo($('#product'))
+        });
+    });
+});
+
+function formatItem(item) {
+    return item.Name + ': $' + item.Price;
+}
+
+function find() {
+    var id = $('#prodId').val();
+    $.getJSON(uri + '/' + id).done(function (data) {
+        $('#product').text(formatItem(data));
+    }).fail(function (jqXHR, textStatus, err) {
+        $('#product').text('Error: ' + err);
+        console.log(err);
+    });
+}
